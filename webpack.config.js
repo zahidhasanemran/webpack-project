9fbfdc40c2ae530c93f5f3af14e1804551c60ca9
Emran <emranhstbq@gmail.com>
2020-10-03 07:54:07 +0600

    error fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const autoprefixer = require('autoprefixer')
+const autoprefixer = require('autoprefixer');
 
 
 module.exports = {
@@ -13,42 +13,44 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.jsx']
     },
-    module: [
-        {
-            test: /\.js$/,
-            loader: 'babel-loader',
-            exclueds: '/node_modules/'
-
-        },
-        {
-            test: /\.css$/,
-            exclueds: '/node_modules/',
-            use: [
-                {loader: 'style-loader'},
-                {loader: 'css-loader', options: {
-                    importLoader: 1,
-                    modules: true,
-                    localIdentName: '[name]__[local]__[hash:base64:5]'
-                }}, // webpack read/parses loader in use array from right to left bottom to top
-                {
-                    loader: "postcss-loader",
-                    options: {
-                        ident: 'postcss',
-                        plugins: () => [
-                            autoprefixer({
-                                "browsers": [
-                                    "> 1%",
-                                    "last 2 versions"
-                                ]
-                            })
-                        ]
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+    
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    {loader: 'style-loader'},
+                    {loader: 'css-loader', options: {
+                        importLoader: 1,
+                        modules: true,
+                        localIdentName: '[name]__[local]__[hash:base64:5]'
+                    }}, // webpack read/parses loader in use array from right to left bottom to top
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            ident: 'postcss',
+                            plugins: () => [
+                                autoprefixer({
+                                    "browsers": [
+                                        "> 1%",
+                                        "last 2 versions"
+                                    ]
+                                })
+                            ]
+                        }
                     }
-                }
-            ]
-        },
-        {
-            test: /\.(png|jpe?g|gif|svg)$/,
-            loader: 'url-loader?limit=8000&name=images/[name].[ext]',
-        }
-    ]
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                loader: 'url-loader?limit=8000&name=images/[name].[ext]',
+            }
+        ]
+    }
 }
